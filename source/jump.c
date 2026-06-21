b131e4be3111a5019bc416f940068620955f969b
James Orr <jwillorr@gmail.com>
2026-06-21 00:14:44 -0500

    fix tile map overlapping with sprite tile mem

--- source/jump.c
+++ source/jump.c
@@ -33,25 +33,18 @@ void init_bg() {
 	// Load tiles into CBB 0
 	memcpy32(&tile_mem[0][0], left_house_tiles, left_house_tiles_len / sizeof(u32));
 	// Load map into SBB 30
-	memcpy32(&se_mem[30][0], left_house_map, left_house_map_len / sizeof(u32));
+	memcpy32(&se_mem[8][0], left_house_map, left_house_map_len / sizeof(u32));
 
     // TODO copy center and right
+    // TODO see if we can fit all 3 BG maps into separate SBB
+    // but keep common tileset
 
     // Setup background and display registers
-    REG_BG0CNT  = BG_CBB(0) | BG_SBB(30) | BG_8BPP | BG_REG_64x64;
-    REG_DISPCNT = DCNT_MODE0 | DCNT_BG0 | DCNT_OBJ | DCNT_OBJ_1D;
+    REG_BG0CNT  = BG_CBB(0) | BG_SBB(8) | BG_8BPP | BG_REG_64x64;
 }
 
 void sprite_loop() {
     OBJ_ATTR *block_obj;
-    // Initialize the person
-    // TODO move into separate init function
-    block_obj = &obj_buffer[0];
-    obj_set_attr(block_obj,
-        ATTR0_TALL | ATTR0_8BPP,
-        ATTR1_SIZE_8x16,
-        ATTR2_PALBANK(0) | 0);
-    obj_set_pos(block_obj, 100, 120);
 
     volatile int frame_counter = 0;
     volatile u8 collision = 0;
@@ -93,14 +86,24 @@ void sprite_loop() {
 int main() {
     while(1) {
         //opening_sequence();
+        init_bg();
+
+        // Initialize the person
+        // TODO move into separate init function
+        //oam_init(obj_buffer, 128);
+        oam_init(obj_buffer, 128);
 
         memcpy32(&tile_mem[4][0], man_tiles, man_tiles_len / sizeof(u32));
         memcpy16(pal_obj_mem, man_pal, man_pal_len / sizeof(u16));
 
-        oam_init(obj_buffer, 128);
-        REG_DISPCNT= DCNT_MODE0 | DCNT_BG0 | DCNT_OBJ | DCNT_OBJ_1D;
+        obj_set_attr(&obj_buffer[0],
+            ATTR0_TALL | ATTR0_8BPP,
+            ATTR1_SIZE_8x16,
+            ATTR2_PALBANK(0) | 0);
+        obj_set_pos(&obj_buffer[0], 100, 120);
 
-        init_bg();
+        REG_DISPCNT = DCNT_MODE0 | DCNT_BG0 | DCNT_OBJ | DCNT_OBJ_1D;
+
         sprite_loop();
 
         // Waiting for new commands
