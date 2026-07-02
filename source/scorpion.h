18f4d79832c7df051968b9b58e7d388e757f03dd
James Orr <jwillorr@gmail.com>
2026-07-01 23:31:28 -0500

    add scorpion sprite

---
+++ source/scorpion.h
@@ -0,0 +1,7 @@
+#define scorpion_tiles_len 512
+extern const unsigned int scorpion_tiles[128];
+#define scorpion_map_len 1024
+extern const unsigned short scorpion_map[512];
+#define scorpion_pal_len 22
+extern const unsigned short scorpion_pal[11];
+extern const unsigned short scorpion_collision_map[512];
