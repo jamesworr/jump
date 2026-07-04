6291e27902d90887dfb7c447cbd69cfdcc07b396
James Orr <jwillorr@gmail.com>
2026-07-03 23:21:43 -0500

    switch to unified palette on sprites

--- source/man.h
+++ source/man.h
@@ -1,4 +1,7 @@
 #define man_tiles_len 128
 extern const unsigned int man_tiles[32];
-#define man_pal_len 10
-extern const unsigned short man_pal[5];
+#define man_map_len 2048
+extern const unsigned short man_map[1024];
+#define man_pal_len 30
+extern const unsigned short man_pal[15];
+extern const unsigned short man_collision_map[1024];
