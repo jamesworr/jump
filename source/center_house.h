35ee8aa23b696bd42555660616533542052e2590
James Orr <jwillorr@gmail.com>
2026-07-11 23:13:09 -0500

    add center house bg and house pig database

---
+++ source/center_house.h
@@ -0,0 +1,7 @@
+#define center_house_tiles_len 14400
+extern const unsigned int center_house_tiles[3600];
+#define center_house_map_len 8192
+extern const unsigned short center_house_map[4096];
+#define center_house_pal_len 36
+extern const unsigned short center_house_pal[18];
+extern const unsigned short center_house_collision_map[4096];
